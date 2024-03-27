f2eb99098541e12e61d9f65d6ea7f8b8f88ff7ae
gokaydervisoglu <gokaydervisoglu@gmail.com>
2024-03-27 19:30:08 +0300

    MainTest.java is updated

--- comic_manager_app/src/test/java/com/mehmetali/comic_manager/MainTest.java
+++ comic_manager_app/src/test/java/com/mehmetali/comic_manager/MainTest.java
@@ -4,7 +4,6 @@ import org.junit.After;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
