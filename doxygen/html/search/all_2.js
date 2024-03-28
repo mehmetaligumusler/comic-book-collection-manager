57644ec2d54dd5b236cad2305d25c362e5fa7f5e
mehmetaligumusler <mehmetaligumusler@users.noreply.github.com>
2024-03-28 20:54:58 +0000

    deploy: c6ee85c40c6bae81b459b9ce5a06672a46ac74e1

--- doxygen/html/search/all_2.js
+++ doxygen/html/search/all_2.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['clearconsole_12',['clearConsole',['../classcom_1_1mehmetali_1_1comic__manager_1_1Main.html#a0755a9c2beb9098786f1365c146b799f',1,'com::mehmetali::comic_manager::Main']]],
-  ['comic_20book_20manager_20application_13',['Comic Book Manager Application',['../md_README.html',1,'']]],
+  ['comic_20book_20manager_20application_13',['Comic Book Manager Application',['../index.html',1,'']]],
   ['comic_5fmanager_14',['comic_manager',['../namespacecom_1_1mehmetali_1_1comic__manager.html',1,'com::mehmetali']]],
   ['control_15',['control',['../classcom_1_1mehmetali_1_1comic__manager_1_1Main.html#ae2508e0386aeaf94ad28320fcce5dddf',1,'com::mehmetali::comic_manager::Main']]],
   ['creditbuyscore_16',['creditbuyscore',['../classcom_1_1mehmetali_1_1comic__manager_1_1UserManager.html#aa627b545ada6c4ef7275780b44e34769',1,'com::mehmetali::comic_manager::UserManager']]],
--- doxygen/html/search/pages_0.js
+++ doxygen/html/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['comic_20book_20manager_20application_259',['Comic Book Manager Application',['../md_README.html',1,'']]]
+  ['comic_20book_20manager_20application_259',['Comic Book Manager Application',['../index.html',1,'']]]
 ];
